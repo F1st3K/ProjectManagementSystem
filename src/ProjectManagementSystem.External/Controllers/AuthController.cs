9868d5ed1f98dedbd152bf957d7264bcd511b416
Nikita Kostin <n1k0s04k@gmail.com>
2025-01-19 01:50:02 +0300

    create logout and register

--- src/ProjectManagementSystem.External/Controllers/AuthController.cs
+++ src/ProjectManagementSystem.External/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 using MediatR;
 using ProjectManagementSystem.Core.Contexts;
+using ProjectManagementSystem.Core.Entities;
+using ProjectManagementSystem.Core.UseCases.Authentication.Commands.Register;
 using ProjectManagementSystem.Core.UseCases.Authentication.Queries.Login;
 
 namespace ProjectManagementSystem.External.Controllers;
@@ -17,7 +19,28 @@ public class AuthController(ISender sender, ICurrentUserContext userContext) : B
         result.Switch(r =>
         {
             userContext.User = r;
-            Console.WriteLine($"---[You have logged in as {r.Role} {r.Name}.]---");
+            Console.WriteLine($"---[You have logged in as {r.Role} {r.Name}]---");
+        }, Problem);
+    }
+
+    public void LogoutCommand(params string[] args)
+    {
+        userContext.User = null;
+    }
+
+    public async void RegisterCommand(params string[] args)
+    {
+        Console.Write("Name: ");
+        var name = Console.ReadLine() ?? string.Empty;
+        Console.Write("Login: ");
+        var login = Console.ReadLine() ?? string.Empty;
+        Console.Write("Password: ");
+        var password = ReadPassword();
+        var result = await sender.Send(new RegisterCommand(name, login, password));
+
+        result.Switch(r =>
+        {
+            Console.WriteLine($"---[You registered new {UserRole.Employee} {name}]---");
         }, Problem);
     }
 
