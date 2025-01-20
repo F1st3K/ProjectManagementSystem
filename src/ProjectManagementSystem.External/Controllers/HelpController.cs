a3fb39e1b3e2b486cae3d8e5bb57d219dbab7ddf
Nikita Kostin <n1k0s04k@gmail.com>
2025-01-21 02:53:53 +0300

    create help

--- src/ProjectManagementSystem.External/Controllers/HelpController.cs
+++ src/ProjectManagementSystem.External/Controllers/HelpController.cs
@@ -4,8 +4,29 @@ public class HelpController : BaseCommandController
 {
     public void Command(params string[] args)
     {
-        Console.WriteLine("Available commands: ");
-        Console.WriteLine("1. create-user - Create a new user");
-        Console.WriteLine("2. help - Show this help message");
+        Console.WriteLine("""
+                          Usage: <command> <subcommand> [...params]
+                          
+                              auth:
+                                  login <Login>? <Password>? - Login system by other user
+                                  logout - Logout system
+                                  register <Name>? <Login>? <Password>? - Create new employee user"
+                                  
+                              user:
+                                  list - List all users
+                                  
+                              project:
+                                  list - List all projects
+                                  create <ProjectName>? - Create new project
+                                  
+                              task:
+                                  list [all? | my? | <UserId>?]? - List all tasks (filter by user id or flags 'my' or 'all')
+                                  create <ProjectId>? <Name>? <Description>? - Create new task
+                                  assign <TaskId>? <UserId>? - Assign task to employee
+                                  change <TaskId>? [ToDo | InProgress | Done]? - Change task status
+                                  
+                              exit - Quit program
+                              help - Show this help message    
+                          """);
     }
 }
